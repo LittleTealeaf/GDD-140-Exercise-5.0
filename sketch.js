d4947b39bd976c5f575d7e195a4b15afa243d80f
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-27 12:17:45 -0500

    Created Checkerboard Pattern

--- sketch.js
+++ sketch.js
@@ -6,4 +6,21 @@ function setup() {
 
 function draw() {
   background(220);
+
+  const rows = 8;
+  const cols = 8;
+
+  const w = width / cols;
+  const h = height / rows;
+
+  for(var r = 0; r < rows; r++) {
+    for(var c = 0; c < cols; c++) {
+      if((r + c)%2==0) {
+        fill('#FFFFFF');
+      } else {
+        fill('#000000');
+      }
+      rect(c * w,r * h,w,h);
+    }
+  }
 }

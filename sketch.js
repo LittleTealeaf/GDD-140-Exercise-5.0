71b9f6f4c85391695386b2ac86a450f03f3d4f0f
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-08 12:38:47 -0500

    Added comments

--- sketch.js
+++ sketch.js
@@ -7,19 +7,19 @@ function setup() {
 function draw() {
   background(220);
 
+  //Rows and cols
   const rows = 8;
   const cols = 8;
 
+  //Set width and height
   const w = width / cols;
   const h = height / rows;
 
+  //Draw
   for(var r = 0; r < rows; r++) {
     for(var c = 0; c < cols; c++) {
-      if((r + c)%2==0) {
-        fill('#FFFFFF');
-      } else {
-        fill('#000000');
-      }
+      //Fill based on the sum's %2 value
+      fill((r + c)%2==0 ? '#FFFFFF' : '#000000');
       rect(c * w,r * h,w,h);
     }
   }
